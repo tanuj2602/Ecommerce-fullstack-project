ce75ea0cc2482dbcafe39e5a8956556db4e39cc8
Tanuj Singh Rathode <tanujsinghrathode@Tanujs-MBP.bbrouter>
2023-01-23 00:31:03 +0530

    add merchant service

---
+++ Services/merchantService/src/test/java/com/example/merchantService/MerchantServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.merchantService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerchantServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
