578a02c7969729d32bf27641bc5d95235f07fb81
Tanuj Singh Rathode <tanujsinghrathode@Tanujs-MacBook-Pro.local>
2023-01-27 11:18:14 +0530

    old files deleted

--- Services/MerchantMicroservice/src/main/java/com/example/MerchantMicroservice/repository/mongo/MerchantRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.MerchantMicroservice.repository.mongo;
-
-import com.example.MerchantMicroservice.entity.Merchant;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MerchantRepository extends MongoRepository<Merchant, String> {
-}
